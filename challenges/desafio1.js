fbb4f66e1c78994613aecb566651282f5cf8a1ea
Filipe de Castro <filipeyoga@gmail.com>
2020-09-16 18:43:07 -0300

    refactor: change find to aggregate

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,21 +1,11 @@
-db.movies.find(
-    {
+db.movies.aggregate([
+  {
+    $match: {
       "imdb.rating": { $gte: 7 },
       genres: { $nin: ["Crime", "Horror"] },
       rated: { $in: ["PG", "G"] },
-      languages: { $all: ["English", "Spanish"] },
+      languages: {
+        $all: ["English", "Spanish"],
+      },
     },
-    { _id: false, title: true, genres: true, rated: true, languages: true }
-  );
-
-// db.movies.aggregate([
-//   {
-//     $match: {
-//       "imdb.rating": { $gte: 7 },
-//       genres: { $nin: ["Crime", "Horror"] },
-//       rated: { $in: ["PG", "G"] },
-//       languages: {
-//         $all: ["English", "Spanish"],
-//       },
-//     },
-//   },
+  },
