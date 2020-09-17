91db4f26a4cfb741f9200d114e9aeb267e4d2d4e
AliceAAtalla <aliceatalla36@gmail.com>
2020-09-17 18:58:51 -0300

    desafio 7 - FIX

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -10,15 +10,21 @@ db.movies.aggregate([
   {
     $group: {
       "_id": "$cast",
-      "numeroFilmes": { $sum: 1 },
-      "mediaIMDB": { $avg: "$imdb.rating" }
+      "mediaIMDB": { $avg: "$imdb.rating" },
+      "numeroFilmes": { $sum: 1 }
     }
   },
   {
     $project: {
       "_id": 1,
-      "numeroFilmes": 1,
-      "mediaIMDB": { $round: ["$mediaIMDB", 1] }
+      "mediaIMDB": { $round: ["$mediaIMDB", 1] },
+      "numeroFilmes": 1
+    }
+  },
+  {
+    $sort: {
+      "numeroFilmes": -1,
+      "_id": -1
     }
   }
 ]);
