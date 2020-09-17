d2db0274310c78aa359939e976d49881531ea2ea
gabriellukke <gabriellukke@outlook.com>
2020-09-17 12:32:52 -0300

    Desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,10 @@
-Kick-Off
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      $and: [ { languages: "English" }, { languages: "Spanish" } ]
+    }
+  }
+]).pretty();
