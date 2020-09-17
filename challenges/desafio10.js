cda76637824a81874eaefa9ad6db421c8c13d7cc
leandromarten1 <leandromarten1@gmail.com>
2020-09-17 17:52:41 -0300

    Fix CC

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -14,7 +14,11 @@ db.trips.aggregate([
     },
   },
   {
-    $project: { _id: 1, duracaoMedia: { $round: ["$duracaoMedia", 2] } },
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
   },
   {
     $sort: { duracaoMedia: 1 },
