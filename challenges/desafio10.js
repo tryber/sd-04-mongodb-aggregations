7d2380969a7a4272e0dc5d92e06c0369e6bde207
leandromarten1 <leandromarten1@gmail.com>
2020-09-17 17:52:11 -0300

    Desafios 10, 11 e 12

---
+++ challenges/desafio10.js
@@ -0,0 +1,22 @@
+// Desafio 10
+
+// Milissegundos p/ Hora => divida o valor de tempo por 3,6e+6
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3.6e6],
+        },
+      },
+    },
+  },
+  {
+    $project: { _id: 1, duracaoMedia: { $round: ["$duracaoMedia", 2] } },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
+  },
+]);
