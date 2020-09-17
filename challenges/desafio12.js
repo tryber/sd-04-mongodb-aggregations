d8b03dcc467a519b16704ee5c91db3ae027dabcf
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-17 20:01:53 -0300

    11 e 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      nomeEstacao: { $push: "$startStationName" },
+      total: { $sum: 1 }
+    }
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $unwind: "$nomeEstacao" },
+  {
+    $group: {
+      _id: "$nomeEstacao",
+      count: { $sum: 1 },
+      total: { $first: "$total" }
+    }
+  },
+  { $sort: { count: -1 } },
+  { $limit: 1 },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } }
+],{ allowDiskUse: true });
