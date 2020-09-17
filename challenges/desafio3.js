67064b8ff31dc6081ba087ce585280f2ec5dce48
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-16 22:43:39 -0300

    desafio 3 - 4

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -11,5 +11,10 @@ db.movies.aggregate([
     $project: {
       _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year"
     }
+  },
+  {
+    $sort: {
+      ano: -1, notaIMDB: -1, titulo: 1
+    }
   }
 ]);
