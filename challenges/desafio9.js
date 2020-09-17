f8ab95136e59cfd443c5e5f3006ffa001395bffb
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-17 19:32:01 -0300

    Desafio 9 feito.

---
+++ challenges/desafio9.js
@@ -0,0 +1,30 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $nin: [""] },
+      birthYear: { $exists: true }
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: {
+        $max: "$birthYear"
+      },
+      menorAnoNascimento: {
+        $min: "$birthYear"
+      },
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: {
+        $toInt: "$maiorAnoNascimento"
+      },
+      menorAnoNascimento: {
+        $toInt: "$menorAnoNascimento"
+      }
+    }
+  }
+]).pretty();
