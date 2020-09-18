95798a931be2ce2350d7f02d723153b7b4703005
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-09-18 17:37:22 -0300

    req6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,16 +1,18 @@
+const favoriteArtists = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
 db.movies.aggregate([
   {
     $match: {
       countries: "USA",
       "tomatoes.viewer.rating": { $gte: 3 },
       cast: {
-        $in: [
-          "Sandra Bullock",
-          "Tom Hanks",
-          "Julia Roberts",
-          "Kevin Spacey",
-          "George Clooney",
-        ],
+        $in: favoriteArtists,
       },
     },
   },
@@ -18,16 +20,7 @@ db.movies.aggregate([
     $addFields: {
       num_favs: {
         $size: {
-          $setIntersection: [
-            "$cast",
-            [
-              "Sandra Bullock",
-              "Tom Hanks",
-              "Julia Roberts",
-              "Kevin Spacey",
-              "George Clooney",
-            ],
-          ],
+          $setIntersection: ["$cast", favoriteArtists],
         },
       },
     },
