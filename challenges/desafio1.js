92d0d59f44a808753c9b82a717be11d69ecb72eb
djonata-dias <diasdjonata@gmail.com>
2020-09-18 17:30:29 -0300

    Desafio 1 pronto

---
+++ challenges/desafio1.js
@@ -0,0 +1,11 @@
+db.movies.aggregate([{
+  $match: {
+    $and: [
+      { "imdb.rating": { $gt: 7 } },
+      { "genres": { $nin: ["Crime", "Horror"] } },
+      { "rated": { $in: ["PG", "G"] } },
+      { "languages": { $in: ["English", "Spanish"] } }
+
+    ]
+  }
+}]);
