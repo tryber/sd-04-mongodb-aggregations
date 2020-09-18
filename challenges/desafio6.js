34b1bfee6e6432853ae5c992e203fa7ec3a49f16
igorgbr <igorgphotoarte@gmail.com>
2020-09-18 02:09:30 -0300

    5 corrigido e 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -12,7 +12,7 @@ db.movies.aggregate([
         maior_rating: { $max: '$imdb.rating' },
         menor_rating: { $min: '$imdb.rating' },
         media_rating: { $avg: '$imdb.rating' },
-        desvio_rating: { $stdDevSamp: '$imdb.rating' },
+        desvio_padrao: { $stdDevSamp: '$imdb.rating' },
       },
     },
     {
@@ -21,7 +21,7 @@ db.movies.aggregate([
         maior_rating: 1,
         menor_rating: 1,
         media_rating: { $round: ['$media_rating', 1] },
-        desvio_rating: { $round: ['$desvio_rating', 1] },
+        desvio_padrao: { $round: ['$desvio_padrao', 1] },
       },
     },
   ]);
