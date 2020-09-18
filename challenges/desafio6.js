e596770ddb4e3a4ce6a7e2ca68a9e9f2c36bf5f8
igorgbr <igorgphotoarte@gmail.com>
2020-09-18 02:57:36 -0300

    n to entendendo

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -2,7 +2,7 @@ db.movies.aggregate([
     {
       $match: {
         awards: {
-          $regex: /^won \d oscar/i, //refatoração baseada no PR do Yoshi (antes estava utilizando 'AND: []')
+          $regex: /won \d oscar/i, //refatoração baseada no PR do Yoshi (antes estava utilizando 'AND: []')
         },
       },
     },
