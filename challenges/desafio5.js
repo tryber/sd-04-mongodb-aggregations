7184bc6195b2e7871a7a4dc72cdfbf88c4bbe305
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-17 23:49:06 -0300

    desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,14 +1,20 @@
 db.movies.aggregate([
     {
-      $match: {
-        $and: [
-          { "tomatoes.viewer.rating": { $gte: 3 } },
-          { "cast": { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] } },
-          { "countries": "USA"}
-        ],
+        $match: {
+          $and: [
+            { "countries": "USA"},
+            { "tomatoes.viewer.rating": { $gte: 3 } },
+            { "cast": {$exists: true}}
+          ],  
+        },
       },
-    },
-    {$project: {_id:0 }},
-    {size: }
-    {$sort: { "num_favs": -1, "tomatoes.viewer.rating": -1, "title": 1 }},
-  ]).pretty();
+    { $addFields: 
+        {"num_favs": 
+        {$size: {$setIntersection: 
+            ["$cast", ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]] 
+        } }}},
+    {$sort: { "num_favs": -1, "tomatoes.viewer.rating": -1, "title": -1 }},
+    {$project: {_id:0, "title":1}},
+    {$skip: 24},
+    {$limit: 1}
+  ]);
