ed07517b21b4802e9ef314f53a4bfdcbf3d36972
leandromarten1 <leandromarten1@gmail.com>
2020-09-18 17:16:12 -0300

    Desafios 8, 13 e 14

---
+++ challenges/desafio13.js
@@ -0,0 +1,31 @@
+// Desafio 13
+
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lte: ISODate("2016-03-10T23:59:59Z"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMedia: {
+        $avg: {
+          $divide: [
+            { $toInt: { $subtract: ["$stopTime", "$startTime"] } },
+            60000,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMedia" },
+    },
+  },
+]);
