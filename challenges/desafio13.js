81fc36493953308e831c6a30ee0e59daa5eced32
Joao Soutto <joaosoutto@hotmail.com>
2020-09-18 12:55:36 -0300

    faltam uns 4 ainda

---
+++ challenges/desafio13.js
@@ -0,0 +1,25 @@
+//referencia link: https://stackoverflow.com/questions/19819870/date-query-with-isodate-in-mongodb-doesnt-seem-to-work
+
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gte: ISODate("2016-03-10") },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
