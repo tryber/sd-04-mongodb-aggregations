0fa270565ac6e413be3e652d5a5309a1f6ad9140
Filipe de Castro <filipeyoga@gmail.com>
2020-09-18 18:17:08 -0300

    feat: challenge 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,23 @@
+const milSecToHour = 1000 * 60 * 60;
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [{ $divide: ["$duracaoMedia", milSecToHour] }, 2],
+      },
+    },
+  },
+]);
