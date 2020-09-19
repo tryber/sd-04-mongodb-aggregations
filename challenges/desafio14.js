0459a696f2e894a893920200235b2fe034a8e45d
igorgbr <igorgphotoarte@gmail.com>
2020-09-19 18:15:48 -0300

    desafio final

---
+++ challenges/desafio14.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      duracaoMediaEmMinutos: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMediaEmMinutos: { $avg: "$duracaoMediaEmMinutos" },
+    },
+  },
+  {
+    $sort: {
+      duracaoMediaEmMinutos: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeid: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+  { $limit: 5 },
+]);
