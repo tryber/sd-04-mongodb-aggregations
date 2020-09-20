342adddeda63c7367e17397c9c36049f35beba0e
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-09-20 18:47:57 -0300

    12 - 80%

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -7,7 +7,7 @@ db.movies.aggregate([
   {
     $group: {
       _id: null,
-      maior_ranting: { $max: "$imdb.rating" },
+      maior_rating: { $max: "$imdb.rating" },
       menor_rating: { $min: "$imdb.rating" },
       media_rating: { $avg: "$imdb.rating" },
       desvio_padrao: { $stdDevSamp: "$imdb.rating" },
@@ -16,7 +16,7 @@ db.movies.aggregate([
   {
     $project: {
       _id: false,
-      maior_ranting: true,
+      maior_rating: true,
       menor_rating: true,
       media_rating: { $round: ["$media_rating", 1] },
       desvio_padrao: { $round: ["$desvio_padrao", 1] },
