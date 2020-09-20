30e380d2b5f0142374fcd0c3911ddc88fd8423bb
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-09-20 03:45:32 -0300

    req13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,15 +1,30 @@
-use("aggregations");
-db.trips.find();
-
-use("aggregations");
-
 db.trips.aggregate([
   {
+    $addFields: {
+      duraçao: { $subtract: ["$stopTime", "$startTime"] },
+    },
+  },
+  {
     $match: {
-      startIme: {
+      startTime: {
         $gte: ISODate("2016-03-10T00:00:00.000Z"),
-        $lte: ISODate("2016-03-10T00:23:59.000Z"),
+        $lte: ISODate("2016-03-10T23:59:59.000Z"),
       },
+    },
+  },
+
+  {
+    $group: {
+      _id: null,
+
+      avg: { $avg: "$duraçao" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+
+      duracaoMediaEmMinutos: { $round: [{ $divide: ["$avg", 60000] }, 0] },
     },
   },
 ]);
