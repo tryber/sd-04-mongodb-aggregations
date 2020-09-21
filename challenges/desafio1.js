3bc9b861470616de3edf7d2603df6ce4839b9a62
Pedro <pedrotpoliveira@gmail.com>
2020-09-20 22:17:03 -0300

    Desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,10 @@
-// First File
+db.movies.aggregate(
+  [{
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      "genres": { $nin: ["Crime", "Horror"] },
+      "rated": { $in: ["PG", "G"] },
+      "languages": { $all: ["English", "Spanish"] }
+    }
+  }]
+);
