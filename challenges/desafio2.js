fab94877b341a61474989c312aca5f539b1ffd79
Thiago Tavares <thiagooo15@gmail.com>
2020-09-22 13:08:09 -0300

    requisito 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,2 +1,22 @@
 // Utilizando o mesmo pipeline anterior, retorne apenas os campos title, rated, imdb.rating,
 // imdb.votes e year, modificando seus nomes para titulo, avaliado, notaIMDB, votosIMDB e ano.
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["G", "PG"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+]);
