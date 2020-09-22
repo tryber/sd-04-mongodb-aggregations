c524e7bbc07e19751f9835e5f62cf8192fa0e004
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-21 22:20:11 -0300

    desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,15 @@
+db.trips.aggregate([
+    {$project: {_id: 0, "diaDaSemana":{$dayOfWeek: "$startTime"}, "startStationName": 1}},
+    {
+        $match: {"diaDaSemana": 5}
+    },
+    {
+        $group: {
+            _id: "$startStationName",
+            "total": {$sum: 1}
+        }
+    },
+    {$sort:{"total": -1}},
+    {$project: { _id: 0, "nomeEstacao": "$_id", "total": "$total" }},
+    {$limit: 1}
+]);
