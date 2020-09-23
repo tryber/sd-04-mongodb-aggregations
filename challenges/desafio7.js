33008b872f28493b715fee1d7710e27bf12634f0
Thiago Tavares <thiagooo15@gmail.com>
2020-09-23 12:36:06 -0300

    requisito 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,2 +1,24 @@
 // Traga o nome do ator ou atriz, número de filmes em que participou e a média do imdb desses
 // filmes com apenas uma casa decimal.
+db.movies.aggregate([
+  { $unwind: "$cast" },
+  {
+    $match: { languages: "English" },
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: { numeroFilmes: -1, _id: -1 },
+  },
+  {
+    $project: {
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+]);
