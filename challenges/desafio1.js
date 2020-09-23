207020265f5df3e347ca4632b1e44499993a6957
Kaecio <kaecio@yahoo.com.br>
2020-09-22 22:46:30 -0300

    Desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,12 @@
-/*começando projeto /
+db.movies.aggregate([
+  {$match:{"imdb.rating":{$gte:7},"genres":{$ne:["Crime","Horror"]},$or:[{"rated":{$eq:"PG"}},{"rated":{$eq:"G"}}],$and:[{languages:"English"},{languages:"Spanish"}]}}
+])
+
+/* Ajude a Trybe a escolher um filme para a próxima noite! 
+Baseado em uma pesquisa, decidimos que os filmes em potencial devem atender aos seguintes critérios:
+imdb.rating deve ser ao menos 7;
+genres não deve conter Crime ou Horror;
+rated deve ser igual a PG ou G;
+languages contém English e Spanish.
+Utilizando a coleção movies, faça um pipeline que retorne todos esses filmes.
+Sua query deve retornar 41 documentos.*/ 
