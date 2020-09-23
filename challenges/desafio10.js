7fbbce7628f03907f11c0b750b2faa6e21217d8c
Thiago Tavares <thiagooo15@gmail.com>
2020-09-23 13:09:01 -0300

    requisito 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,35 +3,38 @@ db.trips.aggregate([
   {
     // add campo "total" com a subtração dos tempos;
     $addFields: {
-      "total": {
+      total: {
         $abs: {
-          $subtract: ["$startTime", "$stopTime"]
-        }
-      }
-    }
+          $subtract: ["$startTime", "$stopTime"],
+        },
+      },
+    },
   },
   {
     // Agrupa: users pelo "usertype", cria campo duração media,
     // com a media do campo "total"
     $group: {
-      "_id": "$usertype",
-      "duracaoMedia": { $avg: "$total" }
-    }
+      _id: "$usertype",
+      duracaoMedia: { $avg: "$total" },
+    },
   },
   {
     // projeta tipo por "_id" e duracaoMedia com result
     // da operação.
     $project: {
-      "_id": 0,
-      "tipo": "$_id",
-      "duracaoMedia": {
-        $round: [{
-          $divide: ["$duracaoMedia", 3600000]
-        }, 2]
-      }
-    }
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [
+          {
+            $divide: ["$duracaoMedia", 3600000],
+          },
+          2,
+        ],
+      },
+    },
   },
   {
-    $sort: { "duracaoMedia": 1 }
-  }
+    $sort: { duracaoMedia: 1 },
+  },
 ]);
--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,1 +1,28 @@
 // Determine qual o dia da semana com maior número de viagens iniciadas.
+
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $group: {
+      _id: "$diaDaSemana",
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { count: -1 },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$count",
+    },
+  },
+]);
