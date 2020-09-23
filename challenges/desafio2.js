66c656e3b44212ab45e27e6553638659b8f3c696
danilossousa <danilosantossousa@gmail.com>
2020-09-23 07:32:37 -0300

    v0.2.0.1

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -16,6 +16,13 @@ db.movies.aggregate([
     }
   },
   {
-    
+    $project: {
+      "_id": 0,
+      "titulo": "$title",
+      "avaliado": "$rated",
+      "notaIMDB": "$imdb.rating",
+      "votosIMDB": "$imdb.votes",
+      "ano": "$year"
+    }
   }
 ]);
