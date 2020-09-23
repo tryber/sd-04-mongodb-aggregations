7b6904f80c95dba34d6f41709012eaf86761d2a8
Thiago Tavares <thiagooo15@gmail.com>
2020-09-23 13:25:04 -0300

    requisito 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,33 @@
 // Agora que você já sabe o dia com mais viagens, determine qual estação tem o maior número de viagens nesse dia da semana.
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    // Referencia resultado desafio 11
+    $match: {
+      diaDaSemana: 5,
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { count: -1 },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$count",
+    },
+  },
+]);
