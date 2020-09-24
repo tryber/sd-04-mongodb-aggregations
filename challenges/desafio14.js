542ba4a8d1ec04dac4984f6c05e88d53d93276d8
Orlando <programadorlando@gmail.com>
2020-09-24 14:00:41 -0300

    desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,33 @@
+const toMinutes = 60 * 1000;
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: false,
+      bikeId: "$_id",
+      duracaoMedia: {
+        $ceil: {
+          $divide: ["$duracaoMedia", toMinutes],
+        },
+      },
+    },
+  },
+]);
