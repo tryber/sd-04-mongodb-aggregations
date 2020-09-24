e918ba7a4172de60626061d025dd2e9f09b4d08c
djonata-dias <diasdjonata@gmail.com>
2020-09-24 07:04:15 -0300

    Desafio 1 e 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,8 +1,8 @@
 db.movies.aggregate([{
-    $match: {
-        "imdb.rating": { $gt: 7 }, as: "notaIMDB",
-        "genres": { $nin: ["Crime", "Horror"] },
-        "rated": { $in: ["PG", "G"] },
-        "languages": { $in: ["English", "Spanish"] }
-    }
+  $match: {
+    "imdb.rating": { $gte: 7 },
+    "genres": { $nin: ["Crime", "Horror"] },
+    "rated": { $in: ["PG", "G"] },
+    "languages": { $in: ["English", "Spanish"] }
+  }
 }]);
