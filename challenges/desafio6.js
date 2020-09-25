2447da602152a16d083d53b5cdd41568406529fe
Victor L <victor.lustosacp@gmail.com>
2020-09-25 19:26:28 -0300

    erro faltando $

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -9,8 +9,8 @@ db.movies.aggregate([
       _id:null,
       "maior_rating": {$max:"$imdb.rating"},
       "menor_rating": {$min:"$imdb.rating"},
-      "media_rating": {$avg:"imdb.rating"},
-      "desvio_padrao": {$stdDevSamp : "imdb.rating"}
+      "media_rating": {$avg:"$imdb.rating"},
+      "desvio_padrao": {$stdDevSamp : "$imdb.rating"}
     }
   },
   {
