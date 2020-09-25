28b1c0a6620a3e4aae89add705ddb5cd5408c840
djonata-dias <diasdjonata@gmail.com>
2020-09-24 23:07:41 -0300

    Desafio 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -5,4 +5,37 @@ db.movies.aggregate([{
     "rated": { $in: ["PG", "G"] },
     "languages": { $in: ["English", "Spanish"] }
   }
-}]);
+},
+{
+  $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year"
+  }
+},
+{ sort: { ano: -1, notaIMDB: -1, titulo: 1 } }
+]);
+
+db.movies.aggregate([{
+  $match: {
+    "imdb.rating": { $gte: 7 },
+    "genres": { $nin: ["Crime", "Horror"] },
+    "rated": { $in: ["PG", "G"] },
+    "languages": { $in: ["English", "Spanish"] }
+  }
+},
+{
+  $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year"
+  }
+}
+]);
+
