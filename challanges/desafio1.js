ab7ceba4a916725a4cbeb94306a381f669e5099f
caiolmf <caio.lmf@gmail.com>
2020-09-28 09:06:57 -0300

    change $in to $all and remove and

--- challanges/desafio1.js
+++ challanges/desafio1.js
@@ -1,12 +1,10 @@
 db.movies.aggregate([
   {
     $match: {
-      $and: [
-        { "imdb.rating": { $gt: 7 } },
-        { genres: { $nin: ["Crime", "Horror"] } },
-        { rated: { $in: ["PG", "G"] } },
-        { languages: { $in: ["English", "Spanish"] } }
-      ],
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
     },
   }
 ]);
