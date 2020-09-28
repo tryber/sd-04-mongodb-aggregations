151e74ce0d1c3d2d73184493e08e306b07d1b2f8
djonata-dias <diasdjonata@gmail.com>
2020-09-28 15:09:14 -0300

    Desafio 4 teste 2

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -2,7 +2,9 @@ db.movies.aggregate([
   {
     $project: {
       _id: 0,
-      "title_split": { $slice: [{ $split: ['$title', " "] }, 0,1] }
+      "title_split": { $slice: [{ $split: ['$title', " "] }, 0, 1] }
     },
+
   },
+  { $sort: { title_split: 1 } }
 ]);
