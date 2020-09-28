267dbff1fa4aaad4fb041b15275504a55dfbdd2e
Pedro <pedrotpoliveira@gmail.com>
2020-09-28 00:16:26 -0300

    Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    "$group": {
+      "_id": "$usertype",
+      "duracaoMedia": {
+        "$avg": {
+          "$divide": [
+            {
+              "$subtract": [
+                "$stopTime", "$startTime"
+              ]
+            }, 1000 * 60 * 60
+          ]
+        }
+      }
+    }
+  }, {
+    "$project": {
+      "_id": 0,
+      "type": "$_id",
+      "duracaoMedia": {
+        "$round": [
+          "$duracaoMedia", 2
+        ]
+      }
+    }
+  }
+]);
