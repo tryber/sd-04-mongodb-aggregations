5a4a79a842500345a309394eace427fcb3fa1765
djonata-dias <diasdjonata@gmail.com>
2020-09-28 17:20:38 -0300

    Desafio 6 teste 2

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,31 +1,29 @@
-const cast = {
-  $setIntersection: [
-    "$cast",
-    [
-      "Sandra Bullock",
-      "Tom Hanks",
-      "Julia Roberts",
-      "Kevin Spacey",
-      "George Clooney"]]
-};
+const names =
+  ["Sandra Bullock",
+    "Tom Hanks",
+    "Julia Roberts",
+    "Kevin Spacey",
+    "George Clooney"];
 
 db.movies.aggregate([
   {
     $match: {
       countries: { $in: ["USA"] },
       "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: 1 }
+    }
+  },
+  {
+    $addFields: {
+      num_favs: { $size: {$setIntersection: [names, cast]}
+       
+      },
     }
   },
   {
     $project: {
       _id: 0,
-      num_favs: {
-        $cond: {
-          if: { $isArray: cast },
-          then: { $size: cast },
-          else: "NA"
-        }
-      }
+      title: 1
     }
   },
   {
