6fb17a65decc0803a2ae0f64760b7d973b1e5204
djonata-dias <diasdjonata@gmail.com>
2020-09-29 16:08:36 -0300

    Desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,31 @@
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  {
+    $lookup: {
+      from: "air_routes",
+      let: { airline: "$airlines", allianceName: "$name" },
+      pipeline: [
+        {
+          $match: {
+            airplane: { $in: ["747", "380"] },
+            $expr: { $eq: ["$airline.name", "$$airline"] }
+          }
+        }
+      ],
+      as: "alianceNames"
+    },
+  },
+  { $unwind: "$alianceNames" },
+  { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+  {
+    $project: {
+      _id: 1,
+      "totalRotas": 1
+    }
+  },
+  {
+    $sort: {
+      totalRotas: 1
+    }
+  }
+]).pretty();
