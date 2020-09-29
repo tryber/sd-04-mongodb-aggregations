f849ee4641d64916f99152010be4ef59ff64abbe
djonata-dias <diasdjonata@gmail.com>
2020-09-29 16:54:19 -0300

    Desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $match: { birthYear: { $ne: "" } }
+  },
+  {
+    $group: {
+      _id: 0,
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: { $toInt: "$maiorAnoNascimento" },
+      menorAnoNascimento: { $toInt: "$menorAnoNascimento" },
+    }
+  }
+]).pretty();
