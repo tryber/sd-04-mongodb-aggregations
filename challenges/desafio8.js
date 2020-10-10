27e9e5ba6aaa488d6a4212c6e54aef87f0245503
fogo5000 <lucaslucas2008@gmail.com>
2020-10-09 22:51:10 -0300

    Requisition 6 - 8 done

---
+++ challenges/desafio8.js
@@ -0,0 +1,22 @@
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  {
+    $lookup: {
+      from: "air_routes",
+      let: { companys: "$airlines" },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$airline.name", "$$companys"] },
+            airplane: { $in: ["747", "380"] },
+          },
+        },
+      ],
+      as: "companysFound",
+    },
+  },
+  { $unwind: "$companysFound" },
+  { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+  { $skip: 2 },
+  { $limit: 1 },
+]);
