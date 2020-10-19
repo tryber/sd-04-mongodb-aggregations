8c2c7e358e7672bad1413f7a3211070378806d47
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2020-10-19 01:41:03 -0300

    Vamos ver se termino

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -14,7 +14,7 @@ db.movies.aggregate([
       _id: 1,
       numeroFilmes: 1,
       mediaIMDB: { $round: ["$mediaIMDB", 1] },
-    }},
+    }}, 
   {$sort: {
       numeroFilmes: -1,
       _id: -1,
