d07343df715fd4786780c13e413065ace0a95ee0
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2020-10-20 12:24:58 -0300

    Igor me salvando novamente

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,37 +1,40 @@
 db.trips.aggregate([
-    {
-        $match: {
-            $and: [{
-                startTime: {$gte: ISODate("2016-03-10")}
-            }]
-        }
+  {
+    $match: {
+      $and: [
+        {
+          startTime: { $gte: ISODate("2016-03-10") },
+        },
+      ],
     },
-    {
-        $addFields: {
-            duracaoMediaEmMinutos: {
-                $divide: [{
-                    $subtract: ["$stopTime", "$startTime"]
-                }, 60000]
-            }
-        }
+  },
+  {
+    $addFields: {
+      duracaoMediaEmMinutos: {
+        $divide: [
+          {
+            $subtract: ["$stopTime", "$startTime"],
+          },
+          60000,
+        ],
+      },
     },
-    {
-        $group: {
-            _id: null,
-            duracaoMediaEmMinutos: {
-                $avg: "$duracaoMediaEmMinutos"
-            }
-        }
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: "$duracaoMediaEmMinutos",
+      },
     },
-    {
-        $project: {
-            _id: 0,
-            duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos"}
-        }
-    }
-])
-
-
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
 
 /* Determine a duração média das viagens iniciadas no dia 10/03/2016,
 em minutos. Arredonde o resultado para cima.
